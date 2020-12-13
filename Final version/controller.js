190a5edba2e723a713ad2c6ca5bf08e126a39afe
Halil İbrahim <53411103+hidirektor@users.noreply.github.com>
2020-12-13 19:03:50 +0300

    Edited !
    • Edited controller.js

--- Final version/controller.js
+++ Final version/controller.js
@@ -4,14 +4,14 @@ app.controller('CVController', function($scope){
     $scope.bilgiEkran = 0;
     $scope.kisi = 0 ;
     $scope.ekip = [
-        {sayi: '1', isim:'Halil İbrahim Direktör', url: 'halil.png'},
-        {sayi: '2', isim:'Mert Alım', url: 'mert.png'},
-        {sayi: '3', isim:'Utku Araal', url: 'utku.png'},
-        {sayi: '4', isim:'Hamza Kaya', url: 'hamza.png'},
-        {sayi: '5', isim:'Umut Can Altıntaş', url: 'umut.png'},
-        {sayi: '6', isim:'Berna Bayraktar', url: 'berna.png'},
-        {sayi: '7', isim:'Ayşe Aleyna Yavuz', url: 'aleyna.png'},
-        {sayi: '8', isim:'Tarık Kıldırıç', url: 'tarık.png'},
+        {sayi: '1', isim:'Halil İbrahim Direktör', url: 'halil.png', sayfa: 'halil.html'},
+        {sayi: '2', isim:'Mert Alım', url: 'mert.png', sayfa: 'mert.html'},
+        {sayi: '3', isim:'Utku Araal', url: 'utku.png', sayfa: 'utku.html'},
+        {sayi: '4', isim:'Hamza Kaya', url: 'hamza.png', sayfa: 'hamza.html'},
+        {sayi: '5', isim:'Umut Can Altıntaş', url: 'umut.png', sayfa: 'umut.html'},
+        {sayi: '6', isim:'Berna Bayraktar', url: 'berna.png', sayfa: 'berna.html'},
+        {sayi: '7', isim:'Ayşe Aleyna Yavuz', url: 'aleyna.png', sayfa: 'aleyna.html'},
+        {sayi: '8', isim:'Tarık Kıldırıç', url: 'tarık.png', sayfa: 'tarik.html'},
 
         
     ];
@@ -19,5 +19,3 @@ app.controller('CVController', function($scope){
 
 
 });
-
-//131212
