a04fbdec874e9703f8ed8080d366704ae33330ff
Utku <30628670+U-Code-U@users.noreply.github.com>
2020-12-13 18:47:57 +0300

    edit
    toparlandı

---
+++ YTK Düzen/controller.js
@@ -0,0 +1,23 @@
+app.controller('CVController', function($scope){
+
+    $scope.ekipEkran = 0;
+    $scope.bilgiEkran = 0;
+    $scope.kisi = 0 ;
+    $scope.ekip = [
+        {sayi: '1', isim:'Halil İbrahim Direktör', url: 'halil.png'},
+        {sayi: '2', isim:'Mert Alım', url: 'mert.png'},
+        {sayi: '3', isim:'Utku Araal', url: 'utku.png'},
+        {sayi: '4', isim:'Hamza Kaya', url: 'hamza.png'},
+        {sayi: '5', isim:'Umut Can Altıntaş', url: 'umut.png'},
+        {sayi: '6', isim:'Berna Bayraktar', url: 'berna.png'},
+        {sayi: '7', isim:'Ayşe Aleyna Yavuz', url: 'aleyna.png'},
+        {sayi: '8', isim:'Tarık Kıldırıç', url: 'tarık.png'},
+
+        
+    ];
+
+
+
+});
+
+//131212
